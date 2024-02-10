7c6dbdad1211832dfdedd3bdf719b9d651dadcfb
zzyin66 <zzyin@uwaterloo.ca>
2024-02-09 23:43:59 -0500

    bind mark as read endpoint to article title

--- client/src/components/lib/NewsCard/NewsCard.tsx
+++ client/src/components/lib/NewsCard/NewsCard.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { NewsFeedItem } from '../../routes/HomePage';
-import './NewsCard.css';
+import React from "react";
+import { NewsFeedItem } from "../../routes/HomePage";
+import "./NewsCard.css";
+import axios from "axios";
 
 interface NewsCardProps {
   item: NewsFeedItem;
@@ -8,28 +9,53 @@ interface NewsCardProps {
 }
 
 export const NewsCard = ({ item, topStory = false }: NewsCardProps) => {
+  const markAsRead = async () => {
+    try {
+      await axios.post("mark_read/", {
+        feed_id: item.id,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <>
       {topStory ? (
-        <div className='top newscard-main'>
-          <div className='top newscard-content'>
-            <span className='subheader'>{item.title}</span>
-            <p className='text' style={{ margin: 0 }}>
+        <div className="top newscard-main">
+          <div className="top newscard-content">
+            <a
+              className="subheader"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={item.url}
+              onClick={markAsRead}
+            >
+              {item.title}
+            </a>
+            <p className="text" style={{ margin: 0 }}>
               {item.description}
             </p>
           </div>
-          <div className='top newscard-media'>
-            <img className='top newscard-media' src={item.image} alt='' />
+          <div className="top newscard-media">
+            <img className="top newscard-media" src={item.image} alt="" />
           </div>
         </div>
       ) : (
-        <div className='newscard-main'>
-          <div className='newscard-media'>
-            <img className='newscard-media' src={item.image} alt='' />
+        <div className="newscard-main">
+          <div className="newscard-media">
+            <img className="newscard-media" src={item.image} alt="" />
           </div>
-          <div className='newscard-content'>
-            <span className='subheader'>{item.title}</span>
-            <p className='text'>{item.description}</p>
+          <div className="newscard-content">
+            <a
+              className="subheader"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={item.url}
+              onClick={markAsRead}
+            >
+              {item.title}
+            </a>
+            <p className="text">{item.description}</p>
           </div>
         </div>
       )}
